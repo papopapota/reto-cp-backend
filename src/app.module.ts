0859c05a3201bb40b259d5792c461b8d6f0936a1
papopapota <daniel_olivares09@hotmail.com>
2026-08-13 15:24:07 -0500

    feat(appModule): integrate Auth, Prisma, and Common modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, PrismaModule, CommonModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
