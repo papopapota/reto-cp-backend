31fadba9a605eacd4d7a374de5451f9a8514868f
papopapota <daniel_olivares09@hotmail.com>
2026-08-14 16:05:01 -0500

    feat(auth): verifyToken

--- src/auth/infrastructure/adapters/jwt-token-service.adapter.ts
+++ src/auth/infrastructure/adapters/jwt-token-service.adapter.ts
@@ -7,6 +7,9 @@ export class JwtTokenServiceAdapter implements TokenServicePort {
     constructor(
         private readonly jwtService: JwtService
     ){}
+    verifyToken(token: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
     generateToken(payload: { userId: string; rol: string; }): Promise<string> {
         return this.jwtService.signAsync(payload);
     }
