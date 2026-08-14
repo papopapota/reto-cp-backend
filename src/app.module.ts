c5a8f5826a3871e63acb5e3d84360655b4183ca7
papopapota <daniel_olivares09@hotmail.com>
2026-08-13 22:23:40 -0500

    refactor(app.module): se agrego userModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/infrastructure/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CommonModule } from './common/common.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, CommonModule],
-  controllers: [],
+  imports: [AuthModule, PrismaModule, CommonModule, UserModule],
+  controllers: [
+    
+  ],
   providers: [],
 })
 export class AppModule {}
