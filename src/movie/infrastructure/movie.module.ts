c63de90765aacb57faa4d1a51566b154eb66cfac
papopapota <daniel_olivares09@hotmail.com>
2026-08-15 12:23:47 -0500

    refactor(movie): s emovio el repositorio

--- src/movie/infrastructure/movie.module.ts
+++ src/movie/infrastructure/movie.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MovieController } from './controllers';
+import { GetMoviesUseCase } from '../application/use-cases';
+import { PrismaMovieRepositoryAdapter } from './adapters';
+import { MOVIE_REPOSITORY_PORT } from '../domain/ports';
 
 @Module({
-  providers: [],
+  providers: [
+    {
+      provide: MOVIE_REPOSITORY_PORT,
+      useClass: PrismaMovieRepositoryAdapter
+    },
+    GetMoviesUseCase
+  ],
   imports: [
   ],
   exports: [],
