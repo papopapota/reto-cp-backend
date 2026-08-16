91c4fb7e3840796b80ae0f14d10beabd28fc4959
papopapota <daniel_olivares09@hotmail.com>
2026-08-16 17:04:12 -0500

    feat(showtime): enhance ShowtimeRepositoryPort with overlapping showtimes and update create method

--- src/showtime/infrastructure/adapters/prisma-showtime-repository.adapter.ts
+++ src/showtime/infrastructure/adapters/prisma-showtime-repository.adapter.ts
@@ -1,18 +1,48 @@
 import { PrismaService } from "src/prisma/prisma.service";
 import { Showtime } from "src/showtime/domain/entities";
-import { ShowtimeFilterOptions, ShowtimeRepositoryPort } from "src/showtime/domain/ports";
+import { ShowtimeFilterOptions, ShowtimeRepositoryPort, ShowtimeWithMovie } from "src/showtime/domain/ports";
 import { ShowtimeMapper } from "../mappers";
 import { Injectable } from "@nestjs/common";
 import { PaginatedResultPort } from "src/common/application/ports";
 import { Prisma } from "@prisma/client";
+import { MovieMapper } from "src/movie/infrastructure/mappers/movie.mapper";
 
 @Injectable()
 export class PrismaShowtimeRepositoryAdapter implements ShowtimeRepositoryPort {
     constructor(
         private readonly prisma: PrismaService
     ) { }
-    create(showtime: Showtime): Promise<void> {
-        throw new Error("Method not implemented.");
+    async findOverlappingShowtimes(room: string, startTime: Date, endTime: Date): Promise<ShowtimeWithMovie[]> {
+        const windowStart = new Date(startTime.getTime() - 4 * 60 * 60 * 1000); // 4 horas antes
+        const windowEnd = new Date(endTime.getTime() + 4 * 60 * 60 * 1000);    // 4 horas después
+
+        const showtimes = await this.prisma.showtimeModel.findMany({
+            where: {
+                room,
+                dateTime: {
+                    gte: windowStart,
+                    lte: windowEnd,
+                },
+                OR: [
+                    { deletedAt: { isSet: false } },
+                    { deletedAt: null },
+                ],
+            },
+            include: {
+                movieModel: true,
+            },
+        });
+
+        return showtimes.map((showtime) => ({
+            showtime: ShowtimeMapper.toDomain(showtime),
+            movie: MovieMapper.toDomain({...showtime.movieModel!}),
+        }));
+    }
+    async create(showtime: Showtime): Promise<Showtime> {
+        const showtimeData = ShowtimeMapper.toPersistence(showtime);
+        return this.prisma.showtimeModel.create({ data: showtimeData }).then((createdShowtime) => {
+            return ShowtimeMapper.toDomain(createdShowtime);
+        });
     }
     findById(id: string): Promise<Showtime | null> {
         throw new Error("Method not implemented.");
