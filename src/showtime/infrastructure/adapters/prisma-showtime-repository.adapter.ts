2d369d09185c20da0cbb0c2a7fe8980fb33b0374
papopapota <daniel_olivares09@hotmail.com>
2026-08-15 20:45:52 -0500

    feat(showtime): implement ShowtimeRepositoryPort and PrismaShowtimeRepositoryAdapter

---
+++ src/showtime/infrastructure/adapters/prisma-showtime-repository.adapter.ts
@@ -0,0 +1,45 @@
+import { PrismaService } from "src/prisma/prisma.service";
+import { Showtime } from "src/showtime/domain/entities";
+import { ShowtimeRepositoryPort } from "src/showtime/domain/ports";
+import { ShowtimeMapper } from "../mappers";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class PrismaShowtimeRepositoryAdapter implements ShowtimeRepositoryPort {
+    constructor(
+        private readonly prisma: PrismaService
+    ) { }
+    create(showtime: Showtime): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    findById(id: string): Promise<Showtime | null> {
+        throw new Error("Method not implemented.");
+    }
+    findAll(): Promise<Showtime[]> {
+        throw new Error("Method not implemented.");
+    }
+    async findUpcomingByMovieId(movieId: string): Promise<Showtime[]> {
+        return this.prisma.showtimeModel.findMany({
+            where: {
+                movieModelId: movieId,
+                dateTime: {
+                    gte: new Date()
+                },
+                deletedAt: null,
+
+            },
+            orderBy: {
+                dateTime: 'asc'
+            }
+        }).then((showtimes) => {
+            return showtimes.map((showtime) => ShowtimeMapper.toDomain(showtime));
+        });
+    }
+    update(showtime: Showtime): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    delete(id: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+}
--- src/showtime/infrastructure/showtime.module.ts
+++ src/showtime/infrastructure/showtime.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { SHOWTIME_REPOSITORY_PORT } from '../domain/ports';
+import { PrismaShowtimeRepositoryAdapter } from './adapters';
 
-@Module({})
+@Module({
+    providers: [
+        {
+            provide: SHOWTIME_REPOSITORY_PORT,
+            useClass: PrismaShowtimeRepositoryAdapter,
+        }
+    ],
+    imports: [],
+    exports: [
+        SHOWTIME_REPOSITORY_PORT
+    ],
+    controllers: [],
+})
 export class ShowtimeModule {}
