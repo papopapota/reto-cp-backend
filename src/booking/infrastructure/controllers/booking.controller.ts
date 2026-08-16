74797702a28322f4c8ea68bbdb5d24a8891c7f85
papopapota <daniel_olivares09@hotmail.com>
2026-08-16 18:53:14 -0500

    feat(booking): enhance BookingModule and BookingController for booking creation functionality

--- src/booking/infrastructure/controllers/booking.controller.ts
+++ src/booking/infrastructure/controllers/booking.controller.ts
@@ -1,16 +1,19 @@
-import { Controller, Get, Param, ParseUUIDPipe, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Query } from '@nestjs/common';
+import { CreateBookingDto } from 'src/booking/application/dtos';
+import { CreateBookingUseCase } from 'src/booking/application/use-cases';
 
 @Controller('bookings')
 export class BookingController {
-    constructor() {
-
+    constructor(
+        private readonly createBookingUseCase: CreateBookingUseCase
+    ) {
     }
 
     @Post()
     createBooking(
-
+        @Body() dto: CreateBookingDto
     ) {
-        return 'Booking';
+        return this.createBookingUseCase.execute(dto);
     }
     @Get()
     getBookingById(
